f9916f8cf71354f1bf09a2ac82da070d14a638b5
lijing <lijing@didachuxing.com>
2021-12-01 17:46:17 +0800

    给 Eureka 注册中心添加认证

---
+++ eureka-security-server/src/main/java/com/example/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * SpringSecurity 配置
+ *
+ * @author lijing
+ * @date 2021/11/30 18:07
+ **/
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
